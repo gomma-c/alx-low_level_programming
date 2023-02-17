944c9974e33c795d1c01a3d775e640ebc47f28c1
gomma-c <gommamsimanga@gmail.com>
2023-02-17 03:26:19 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
- * main - A main function that will generate a negative number.
+ * main - A program tha prints a postive or negative number.
  * Return: Always 0.
- *
+ * 
  */
 
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+        
         if (n > 0)
 	{
-	printf("%d is postive\n", n);	
-	}
-	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
+	     printf("%d int is positive\n"');
         }
-        else
+        else if (n == 0)
+        }         
+             printf("%d is zero\n"');
+        {
+        else (n < 0)
+	} 
+             printf('"%d is negative\n"');
 	{	
-	printf("%d is negative\n", n);
-        }
- 	return (0);
-}	
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program thats prints a text accoring.
+ *
+ */
+
+int main(void)
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastd);
+	}
+	return (0);
+}    
