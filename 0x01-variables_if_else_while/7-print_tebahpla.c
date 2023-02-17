75b28f38d4442f0d0c16d7cca7fa91650ce2d0ca
gomma-c <gommamsimanga@gmail.com>
2023-02-17 11:06:49 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,19 +3,17 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point.
  * Return: Always 0.
  *
  */
 
 int main(void)
-
 {
 	char low;
 
 	for (low = 'z'; low >= 'a'; low--)
 	putchar(low);
 	putchar('\n');
-
 	return (0);
 }	
