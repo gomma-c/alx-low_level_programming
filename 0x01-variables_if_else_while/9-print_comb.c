88041a0d888437cf30fea4ad41f48f41cf9a2541
gomma-c <gommamsimanga@gmail.com>
2023-02-17 18:15:22 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,13 +15,13 @@ int main(void)
 
 	for (d = '0'; d <= '9'; d++)
 	{
-	putchar(d);
-	if (d != '9')
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
-}	
+}
