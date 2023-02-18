711a15e229c0edb25a2602ae40ccd659a3484c74
gomma-c <gommamsimanga@gmail.com>
2023-02-17 17:48:04 -0800

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - A program thats prints a text accoring to positive and negative.
- *Return: Always 0.
+ * main - Entry point 
+ * Return: Always 0.
  *
  */
 
@@ -19,16 +19,15 @@ int main(void)
 
 	if (lastd > 5)
 	{
-		printf("Last digit of %d is %d and greater than 5\n", n, lastd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastd);
+		printf ("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastd);
 	}
 	return (0);
-
 }
