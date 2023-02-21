eb832a54448f53953fa77b4c2499cfacdd14dac8
gomma-c <gommamsimanga@gmail.com>
2023-02-21 03:29:37 -0800

    5-sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islapha - Checks for alphabetic character
+ * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else 
+ * Return: 1 for alphabetic character or 0 for anything else. 
  */
 
 int _isalpha(int c)
