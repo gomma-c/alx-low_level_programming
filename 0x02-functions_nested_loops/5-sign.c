27845fda63e4313f08c698521098be8dc46c5d02
gomma-c <gommamsimanga@gmail.com>
2023-02-21 10:25:51 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anything else 
+ * Return: 1 .
  */
 
 int print_sign(int n)
@@ -19,7 +19,7 @@ int print_sign(int n)
 		_putchar(45);
 		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar(48);
 		return (0);
