691b43b5b4c7d8e3f1eaf333f3d8d1eec6304a89
gomma-c <gommamsimanga@gmail.com>
2023-03-06 09:02:15 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] >= '\0'; i++)
-{
-	if (s[i] == c)
-	
-		return (s + 1);
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
