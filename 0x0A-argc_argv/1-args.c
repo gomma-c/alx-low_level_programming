65a7a68712e445f7ab929049291e0bf1617aa6ad
gomma-c <gommamsimanga@gmail.com>
2023-03-10 06:58:53 -0800

    ./mynewnameis

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of args.
+ * main - Prints the number of args.
  * @argc: argument count.
  * @argv: argument vector.
  * Return: Always 0.
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAINN_H
+#define MAIN_H
+
+int main(int argc, char *argv[]);
+
+#endif
